56ab5eef03544c7a34deca30b6991f7e0c8079c2
nyaataco <63835195+nyaataco@users.noreply.github.com>
2021-06-01 16:32:06 +0900

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist
 
-# SECURITY WARNING: keep the secret key used in production secret!
 #####
 
 # SECURITY WARNING: don't run with debug turned on in production!
